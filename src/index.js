d60460278061bcfa1ca2084bb870d78e4437b421
ad222kr <alexdriagin12@gmail.com>
2017-01-11 20:57:06 +0100

    check if a repo already exists on the user

--- src/index.js
+++ src/index.js
@@ -1,41 +1,29 @@
 #!/usr/bin/env node
+// @flow
 require('dotenv').config()
 require('babel-polyfill')
 
-// @flow
-import Promise from 'bluebird'
-import fetch from 'node-fetch'
 import dotenv from 'dotenv'
 import program from 'commander'
 import co from 'co'
 import prompt from 'co-prompt'
-
-import {GITHUB_API_BASE_URL, getHeaders} from './utils'
-
-
-
-function createRepo({name, isPrivate}: {name: string, isPrivate: boolean}): Promise<any> {
-  const headers = getHeaders()
-  const body = JSON.stringify({
-    name,
-    private: isPrivate
-  })
-
-  return fetch(
-    `${GITHUB_API_BASE_URL}/user/repos`, 
-    {method: 'POST', headers, body}
-  )
-  .then(res => res.json())
-}
+import chalk from 'chalk'
+import {createRepo, checkIfRepoExists} from './lib/github'
 
 function main(name: string): void {
   co(function *() {
-    console.log('heh')
+    const exists = yield checkIfRepoExists(name)
+    console.log(name)
+    console.log(exists)
+    if (exists) {
+      console.log(chalk.bold.red(`A repository with the name ${name} already exists on your github account..`))
+      process.exit(0)
+    }
     const isPrivate = yield prompt('Private repo? y/N: ')
     const description = yield prompt('Description: ')
-    const repo = yield createRepo({name, isPrivate: isPrivate === 'y'})
+    const repo = yield createRepo({name, isPrivate: isPrivate === 'y', description})
     const {html_url} = repo
-    console.log(`Your newly created repository is created and located at ${html_url}`)
+    console.log(`Your newly created repository is created and located at ${chalk.bold.green(html_url)}`)
   })
 }
 
@@ -44,8 +32,14 @@ program
   .action(main)
   .parse(process.argv);
 
+
+
 if (!program.args[0]) {
   console.log('Please supply a name for the repository')
   process.exit(0)
 }
+
+
+
+
 
---
+++ src/lib/github.js
@@ -0,0 +1,34 @@
+import fetch from 'node-fetch'
+import {GITHUB_API_BASE_URL, getHeaders} from './utils'
+
+export function createRepo(
+  {name, isPrivate}: {name: string, isPrivate: boolean}
+): Promise<any> {
+  const headers = getHeaders()
+  const body = JSON.stringify({
+    name,
+    private: isPrivate
+  })
+
+  return fetch(
+    `${GITHUB_API_BASE_URL}/user/repos`, 
+    {method: 'POST', headers, body}
+  )
+  .then(res => res.json())
+  .then(res => {
+    console.log(res)
+    return res
+  })
+}
+
+export function checkIfRepoExists(name: string): Promise<any> {
+  const headers = getHeaders()
+  
+  return fetch(
+      `${GITHUB_API_BASE_URL}/user`, 
+      {method: 'GET', headers}
+    )
+    .then(res => res.status !== 404)
+}
+
+

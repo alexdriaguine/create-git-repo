ca23727dee59429d0ed0a84df45872df789801a6
DovidM <landyx47@gmail.com>
2017-10-17 15:16:05 -0400

    Removed unneeded plugin

--- src/backpack.config.js
+++ src/backpack.config.js
@@ -1,6 +1,3 @@
-const chmod = require('chmod');
-var WebpackOnBuildPlugin = require('on-build-webpack');
-
 module.exports = {
   webpack: (config, options, webpack) => {
     config.entry.main = [
@@ -15,22 +12,8 @@ module.exports = {
       {
         test: /\.ts$/,
         loader: 'awesome-typescript-loader',
-        plugins: [
-      new WebpackOnBuildPlugin(function(stats) {
-        console.log('OVER THERE IS THE CORRECT PATH TO GRAPES')
-        chmod("./dist/index.js", 500);
-      })
-
-        ]
       }
     );
-
-    config.module.rules.plugins.push(
-      new WebpackOnBuildPlugin(function(stats) {
-        console.log('OVER THERE IS THE CORRECT PATH TO GRAPES')
-        chmod("./dist/index.js", 500);
-      })
-    )
 
     return config
   }

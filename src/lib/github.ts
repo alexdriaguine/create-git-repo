dc1a9ae2c46a06197731a27892b9e4aac2b5b876
sven <svengiebel@me.com>
2018-10-08 17:17:33 +0200

    #11 handle missing permissions / errors on repo create

--- src/lib/github.ts
+++ src/lib/github.ts
@@ -1,7 +1,6 @@
-
-import fetch from 'node-fetch'
-import {GITHUB_API_BASE_URL, getHeaders} from './utils'
-import {GithubRequestParams} from './entities'
+import fetch from 'node-fetch';
+import {GITHUB_API_BASE_URL, getHeaders, handleErrors} from './utils';
+import {GithubRequestParams} from './entities';
 
 export function createRepo({
   name,
@@ -9,18 +8,24 @@ export function createRepo({
   description,
   accessToken,
 }: GithubRequestParams): Promise<any> {
-  const headers = getHeaders(accessToken)
+  const headers = getHeaders(accessToken);
   const body = JSON.stringify({
     name,
     private: isPrivate,
     description,
-  })
+  });
 
   return fetch(`${GITHUB_API_BASE_URL}/user/repos`, {
     method: 'POST',
     headers,
     body,
-  }).then(res => res.json())
+  }).then(res => {
+    // handle error cases with util function
+    if (res.status >= 400) {
+      return handleErrors(res);
+    }
+    return res.json();
+  });
 }
 
 export function checkIfRepoExists(
@@ -28,10 +33,10 @@ export function checkIfRepoExists(
   accessToken: string,
   username: string,
 ): Promise<{
-    wrongCredentials: boolean;
-    repoExists: boolean;
+  wrongCredentials: boolean;
+  repoExists: boolean;
 }> {
-  const headers = getHeaders(accessToken)
+  const headers = getHeaders(accessToken);
 
   return fetch(`${GITHUB_API_BASE_URL}/repos/${username}/${name}`, {
     method: 'GET',
@@ -39,5 +44,5 @@ export function checkIfRepoExists(
   }).then(({status}) => ({
     wrongCredentials: status === 401,
     repoExists: status === 200,
-  }))
+  }));
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,47 +1,48 @@
-import {exec} from 'child_process'
-import {GithubRequestHeaders, InitRepo} from './entities'
-import * as readline from 'readline'
+import {exec} from 'child_process';
+import {GithubRequestHeaders, InitRepo} from './entities';
+import * as readline from 'readline';
+import * as chalk from 'chalk';
 
-export const GITHUB_API_BASE_URL: string = 'https://api.github.com'
+export const GITHUB_API_BASE_URL: string = 'https://api.github.com';
 
 export const getBasicAuthToken = (
   username: string,
   tokenOrPassword: string,
 ): string =>
-  new Buffer(`${username}:${tokenOrPassword}`, 'ascii').toString('base64')
+  new Buffer(`${username}:${tokenOrPassword}`, 'ascii').toString('base64');
 
-export const getEnvVar = (key: string): string => process.env[key] || ''
+export const getEnvVar = (key: string): string => process.env[key] || '';
 
 export const getHeaders = (accessToken: string): GithubRequestHeaders => {
   return {
     Accept: 'application/vnd.github.v3+json',
     Authorization: `Basic ${accessToken}`,
-  }
-}
+  };
+};
 
 const execute = (dir: string) => (command: string) => () =>
   new Promise((resolve, reject) =>
     exec(command, {cwd: dir}, err => (err ? reject(err) : resolve())),
-  )
+  );
 
 export type InitRepoArgs = {
-  dir: string
-  name: string
-  remoteUrl: string
-}
+  dir: string;
+  name: string;
+  remoteUrl: string;
+};
 
 export const hasCreateReactApp = () =>
   execute('.')('create-react-app --version')()
     .then(() => true)
-    .catch(() => false)
+    .catch(() => false);
 
 export function initiateRepo({dir, name, remoteUrl}: InitRepoArgs): InitRepo {
-  const init = execute(dir)('git init')
-  const createReactApp = execute(dir)(`create-react-app .`)
-  const createReadme = execute(dir)(`echo "# ${name}" >> README.md`)
-  const add = execute(dir)('git add .')
-  const addRemote = execute(dir)(`git remote add origin ${remoteUrl}`)
-  const commit = execute(dir)('git commit -m "first commit"')
+  const init = execute(dir)('git init');
+  const createReactApp = execute(dir)(`create-react-app .`);
+  const createReadme = execute(dir)(`echo "# ${name}" >> README.md`);
+  const add = execute(dir)('git add .');
+  const addRemote = execute(dir)(`git remote add origin ${remoteUrl}`);
+  const commit = execute(dir)('git commit -m "first commit"');
 
   return {
     init,
@@ -50,60 +51,68 @@ export function initiateRepo({dir, name, remoteUrl}: InitRepoArgs): InitRepo {
     add,
     commit,
     addRemote,
-  }
+  };
 }
 
 export const prompt = (function() {
   let rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
-  })
+  });
 
   return function run(question: string, options: {masked?: boolean} = {}) {
-    let stdin: NodeJS.Socket
+    let stdin: NodeJS.Socket;
 
     function onData(char: string) {
-      char = char + ''
+      char = char + '';
       switch (char) {
         case '\n':
         case '\r':
         case '\u0004':
-          stdin.removeListener('data', onData)
-          break
+          stdin.removeListener('data', onData);
+          break;
         default:
           process.stdout.write(
             '\u001b[2K\u001b[200D' +
               question +
               Array(rl['line'].length + 1).join('*'),
-          )
-          break
+          );
+          break;
       }
     }
 
     return function(done: (err: any, answer: string) => void) {
       if (options.masked) {
-        rl.close()
-        stdin = process.openStdin()
+        rl.close();
+        stdin = process.openStdin();
         rl = readline.createInterface({
           input: stdin,
           output: process.stdout,
-        })
-        stdin.on('data', onData)
+        });
+        stdin.on('data', onData);
       }
 
       rl.question(question, answer => {
         if (options.masked) {
-          stdin.removeListener('data', onData)
-          rl.close()
+          stdin.removeListener('data', onData);
+          rl.close();
 
           rl = readline.createInterface({
             input: process.stdin,
             output: process.stdout,
-          })
+          });
         }
 
-        done(null, answer)
-      })
-    }
-  }
-})()
+        done(null, answer);
+      });
+    };
+  };
+})();
+
+// handle all errors that may result while creating repo
+export const handleErrors = async function(body) {
+  const {message, documentation_url} = await body.json();
+  console.log(chalk.bold.red(`Hey! ${message}`));
+  console.log(chalk.blue(`Read more: ${documentation_url}`));
+  process.exit(0);
+};
